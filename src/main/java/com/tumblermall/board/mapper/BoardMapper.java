a7c2c3f4d8635108170e4c4724e68882826f93c2
DESKTOP-G02EQEN\monae <rrr6964@naver.com>
2025-06-26 17:44:13 +0900

    feat(board): 게시판 목록 페이징 처리 1차 구현 완료
    - PageHandler 적용 및 LIMIT 기반 페이징 처리 todo로 남김
    - 기존 전체조회 방식 제거 및 /board 경로로 통합은 페이징 처리 todo하면서 진행할 예정
    - Controller/Service/Mapper 전반 수정
    - JSP 연결 및 기본 리스트 출력 확인
    - 미완성 기능은 주석 TODO로 표시

--- src/main/java/com/tumblermall/board/mapper/BoardMapper.java
+++ src/main/java/com/tumblermall/board/mapper/BoardMapper.java
@@ -2,6 +2,7 @@ package com.tumblermall.board.mapper;
 
 import com.tumblermall.board.dto.BoardContextDto;
 import com.tumblermall.board.dto.BoardMainDto;
+import com.tumblermall.board.dto.PageHandler;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -15,9 +16,23 @@ public interface BoardMapper {
     //게시판main (게시물 목록 리스트)
     List<BoardMainDto> boardMain ();
 
+
     //게시글 본문
+    Integer viewUp(Integer postId);
     List<BoardContextDto> showContext(Integer postId);
 
+// todo
+//    //페이징
+//    List<BoardMainDto> getPagedPosts(PageHandler Handler);
 
+// todo
+//    //관리자 작성
+//    int noticemain(AdminWriteVO vo);
+//    int noticecontext(AdminWriteVO vo);
+//    int noticehistory(AdminWriteVO vo);
+
+// todo
+//    //고객 작성
+//    List<UserWriteVo> userWrite();
 
 }
---
+++ src/main/java/com/tumblermall/board/vo/AdminWriteVO.java
@@ -0,0 +1,135 @@
+package com.tumblermall.board.vo;
+public class AdminWriteVO {
+
+// todo
+//    post
+//    private String title;
+//    private String writer;
+//    private Date eventStart;
+//    private Date eventEnd;
+//    private String nowEvent;
+//
+//    public String getTitle() {
+//        return title;
+//    }
+//
+//    public void setTitle(String title) {
+//        this.title = title;
+//    }
+//
+//    public String getWriter() {
+//        return writer;
+//    }
+//
+//    public void setWriter(String writer) {
+//        this.writer = writer;
+//    }
+//
+//    public Date getEventStart() {
+//        return eventStart;
+//    }
+//
+//    public void setEventStart(Date eventStart) {
+//        this.eventStart = eventStart;
+//    }
+//
+//    public Date getEventEnd() {
+//        return eventEnd;
+//    }
+//
+//    public void setEventEnd(Date eventEnd) {
+//        this.eventEnd = eventEnd;
+//    }
+//
+//    public String getNowEvent() {
+//        return nowEvent;
+//    }
+//
+//    public void setNowEvent(String nowEvent) {
+//        this.nowEvent = nowEvent;
+//    }
+//
+//    //boardCode
+//    private String bardCode;
+//
+//    public String getBardCode() {
+//        return bardCode;
+//    }
+//
+//    public void setBardCode(String bardCode) {
+//        this.bardCode = bardCode;
+//    }
+//
+//    //saveContext
+//    private String context;
+//
+//    public String getContext() {
+//        return context;
+//    }
+//
+//    public void setContext(String context) {
+//        this.context = context;
+//    }
+//
+//    //history
+//    private Date writeAt;
+//    private String nowStatus;
+//
+//    public Date getWriteAt() {
+//        return writeAt;
+//    }
+//
+//    public void setWriteAt(Date writeAt) {
+//        this.writeAt = writeAt;
+//    }
+//
+//    public String getNowStatus() {
+//        return nowStatus;
+//    }
+//
+//    public void setNowStatus(String nowStatus) {
+//        this.nowStatus = nowStatus;
+//    }
+//
+//    //viewCount
+//    private Integer viewCount;
+//
+//    public Integer getViewCount() {
+//        return viewCount;
+//    }
+//
+//    public void setViewCount(Integer viewCount) {
+//        this.viewCount = viewCount;
+//    }
+//
+//    //notice
+//    private String isPinned;
+//
+//    public String getIsPinned() {
+//        return isPinned;
+//    }
+//
+//    public void setIsPinned(String isPinned) {
+//        this.isPinned = isPinned;
+//    }
+//
+//    //attached
+//    private String filename;
+//    private String fileUrl;
+//
+//    public String getFilename() {
+//        return filename;
+//    }
+//
+//    public void setFilename(String filename) {
+//        this.filename = filename;
+//    }
+//
+//    public String getFileUrl() {
+//        return fileUrl;
+//    }
+//
+//    public void setFileUrl(String fileUrl) {
+//        this.fileUrl = fileUrl;
+//    }
+}
